a6b5b16b3df45af3bdaff4f2df5a43060bbd6eb2
William Robersone <william.robersone@gmail.com>
2021-01-21 11:05:10 -0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  useEffect(() => {
+    registerForPushNotification().then(token=>console.log(token)).catch
+    (err => console.log(err))
+  },[])
+
+  async function registerForPushNotification() {
+    const {status} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    if (status!='granted') {
+      const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+    }
+    if (status !='granted') {
+      alert('Fail to get the push token');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    return token;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! I may add a button soon.</Text>
       <StatusBar style="auto" />
     </View>
   );
